0372d9fce85e62548bf2a984531b94953787f955
JuNyeong <91647220+junyeong7138@users.noreply.github.com>
2025-08-02 01:35:38 +0900

    Add files via upload

---
+++ 9. Wire Declare.v
@@ -0,0 +1,30 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n
+); 
+
+    wire or_in;
+
+    assign or_in = (a && b) || (c && d);
+
+    assign out = or_in;
+    assign out_n = ~or_in;
+
+    /*
+    Solution
+
+    wire w1, w2;		// Declare two wires (named w1 and w2)
+	assign w1 = a&b;	// First AND gate
+	assign w2 = c&d;	// Second AND gate
+	assign out = w1|w2;	// OR gate: Feeds both 'out' and the NOT gate
+
+	assign out_n = ~out;	// NOT gate
+
+    */
+
+endmodule
